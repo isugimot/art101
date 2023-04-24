a4683db7be750d1902537b29bc5e2899b0ad4e0a
Ichiro Sugimoto <isugimot@ucsc.edu>
2023-04-23 20:43:00 -0700

    Lab 4 Pseudo code

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// An implementation of the game
+// Ichiro Sugimoto <isugimot@ucsc.edu>
+// April 23 2023
 
 // Constants
+//Hand_player
+//Hand_computer
 
 // Functions
+//Take in inputs from player, 1, 2, or 3. Each number is connected to the input Rock, Paper, and Scissors
+//Get an random value from 1 to 3 using rand() function and put it into Hand_computer
+//If the two variables Hand_player and Hand_computer are equal
+  //print out on screen that the game is tied
+  //Jump back to the first line
+//Else if Hand_player == to (Hand_computer - 1) or (Hand_player == 3 and Hand_computer == 1)
+  //print out on screen that the player has losed
+  //Jump back to the first line
+//Else
+  //print out on screen that the player has won
+  //Jump back to the first line
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
