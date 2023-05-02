babc25b6080ca7c9ebc31ad3b739a0de38639164
Ichiro Sugimoto <isugimot@ucsc.edu>
2023-05-01 18:31:24 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,9 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Working with arrays and objects on JavaScript
+// Author: Ichiro Sugimoto
+// Date: 5/1/2023
 
 // Constants
+const myTransport = ["Bus", "Car", "Walking"]
 
+var myMainRide = {
+  make: "Honda", 
+  model: "Honda E", 
+  color: "Black", 
+  year: 2020, 
+  age: function() {
+    return 2023 - this.year;
+  }
+}
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
@@ -14,6 +24,9 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
+  document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+  document.writeln("My Main Ride: <pre>",
+      JSON.stringify(myMainRide, null, '\t'), "</pre>");
   // the code that makes everything happen
 }
 
